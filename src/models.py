564fd541cde1e766d399ecefec1f825697c6cb83
Andidela <andidela83@gmail.com>
2024-12-04 01:23:52 +0000

    Relationships hechas

--- src/models.py
+++ src/models.py
@@ -7,11 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id= Column(Integer, primary_key=True)
-    user_to_id= Column(Integer, ForeignKey('user.id'))
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -20,17 +15,28 @@ class User(Base):
     lastname= Column(String(250), nullable=False)
     email= Column(String(250), nullable=False)
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id= Column(Integer, primary_key=True)
+    user_from_id= Column(Integer, ForeignKey('user.id'))
+    user_to_id= Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey('user.id'))
+    user= relationship(User)
+
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text= Column(String(250), nullable=False)
     author_id= Column(Integer, ForeignKey('user.id'))
     post_id= Column(Integer, ForeignKey('post.id'))
+    user= relationship(User)
+    post= relationship(Post)
 
-class Post(Base):
-    __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id= Column(Integer, ForeignKey('user.id'))
 
 class Media(Base):
     __tablename__ = 'media'
@@ -38,17 +44,9 @@ class Media(Base):
     type= Column(Enum('image','video','audio',name='media_types'))
     url= Column(String(250), nullable=False)
     post_id= Column(Integer, ForeignKey('post.id'))
+    post= relationship(Post)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
 
     def to_dict(self):
         return {}
